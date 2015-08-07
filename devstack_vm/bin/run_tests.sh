20a4f7b44beb5eef300c8b7b07a118394223c644
Alin Balutoiu <abalutoiu@cloudbasesolutions.com>
2015-08-07 19:18:12 +0300

    Adds deploy devstack to VM

--- devstack_vm/bin/run_tests.sh
+++ devstack_vm/bin/run_tests.sh
@@ -11,15 +11,15 @@ EXCLUDED_TESTS="$TEMPEST_DIR/excluded_tests.txt"
 RUN_TESTS_LIST="$TEMPEST_DIR/test_list.txt"
 mkdir -p "$TEMPEST_DIR"
 
-# Checkout  stable commit for tempest to avoid possible
+# Checkout stable commit for tempest to avoid possible
 # incompatibilities for plugin stored in Manila repo.
 TEMPEST_COMMIT="489f5e62"  # 15 June, 2015
 git checkout $TEMPEST_COMMIT
 
 # Install Manila Tempest integration
-cp -r /home/manila/contrib/tempest/tempest/* $TEMPEST_BASE/tempest
+cp -r /home/ubuntu/manila/contrib/tempest/tempest/* $TEMPEST_BASE/tempest
 
-# run all manila tests, api + scenario tests
+# run all manila tests, api and scenario tests
 testr list-tests | grep share | grep -v test_image > "$RUN_TESTS_LIST" || echo "failed to generate list of tests"
 
 testr run --parallel --subunit --load-list=$RUN_TESTS_LIST | subunit-2tol > /home/ubuntu/tempest/subunit-output.log 2>&1
